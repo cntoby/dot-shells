dc586852aabeaf0a3eec6e7485f3236adf5d8bb1
Toby <cntoby@gmail.com>
2016-06-03 01:27:29 +0800

    pretty output

--- docker.sh
+++ docker.sh
@@ -3,16 +3,20 @@ if which docker-machine > /dev/null; then
     if [ "$VM" != "" ]; then
 	STATUS=$(docker-machine status $VM) # get status of the default vm
 	if [ "$STATUS" == "Stopped" ]; then # if the vm stopped then start it
-	    docker-machine start $VM 2>/dev/null
+	    printf "Starting ${VM} "
+	    docker-machine start $VM > /dev/null
 	    if [ "$?" == "0" ]; then
+		printf "Done\n"
 		VM_STARTED=1
 	    else
+		print "Failed\n"
 		VM_STARTED=0
 	    fi
 	else
 	    VM_STARTED=1
 	fi
 	if [ "$VM_STARTED" == "1" ]; then # if the vm start success of started already, set env in current terminal.
+	    echo "Set docker envroment"
 	    eval $(docker-machine env $VM)
 	fi
     else
